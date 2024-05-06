7eb3edd00c04f84d4ad742068e07c2378845ca7b
Federico Bussi <federico.bussi.06@gmail.com>
2024-05-06 12:43:54 +0200

    Fixato un bug che faceva leggere una sola parola

--- contavocali.c
+++ contavocali.c
@@ -4,11 +4,16 @@
 int main(){
 	char s[256];
 	printf("Inserisci una stringa: ");
-	scanf("%s",s);
+	leggiStringa(s,256);
 	int conta=0;
 	for(int i=0;i<strlen(s);i++){
-		if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') conta++;
+		if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') conta++;
 	}
 	printf("Ci sono %d vocali\n",conta);
 	return 0;
+}
+
+void leggiStringa(char s[],int len){
+	fgets(s,len,stdin);
+	s[strlen(s)-1]='\0';
 }
